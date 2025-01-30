ce37b83a59b6b268d49b3b0f305f4f2c45b7c066
Bhanu1201 <bhanu@dvk.ai>
2025-01-30 19:10:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 10000;
 
 // Middleware
+app.use(cors());
 app.use(cors({ origin: "https://client-sso-frontend.onrender.com", credentials: true }));
 app.use(bodyParser.json());
 
